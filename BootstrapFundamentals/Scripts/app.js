cc9bd814bfe4d3cd863439ea4e79d3e9d9563e46
Mesut OZTURK <mesutoztrk@msn.com>
2018-02-02 19:32:52 +0300

    ui

--- BootstrapFundamentals/Scripts/app.js
+++ BootstrapFundamentals/Scripts/app.js
@@ -1,1 +1,15 @@
-﻿
+﻿/// <reference path="jquery-3.3.1.js" />
+/// <reference path="jquery-ui-1.12.1.js" />
+/// <reference path="bootstrap.js" />
+
+$(function () {
+    $("#slider-range").slider({
+        range: true,
+        min: 0,
+        max: 500,
+        values: [50, 100],
+        slide: function (event, ui) {
+            $("#amount").val("$" + ui.values[0] + " - $" + ui.values[1])
+        }
+    });
+});
